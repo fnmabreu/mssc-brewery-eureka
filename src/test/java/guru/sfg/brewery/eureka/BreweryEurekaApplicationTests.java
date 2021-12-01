770b5f42008b91c69805469295dc655383f5d187
fnmabreu@gmail.com <fnmabreu@gmail.com>
2021-12-01 22:12:28 +0000

    Eureka service creation

---
+++ src/test/java/guru/sfg/brewery/eureka/BreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.sfg.brewery.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
