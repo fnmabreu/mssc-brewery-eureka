d8403f02a34edae2a7132e115027ed0f50bcd34a
fnmabreu@gmail.com <fnmabreu@gmail.com>
2021-12-05 11:57:49 +0000

    Add HTTP Basic Auth Using Spring Security

---
+++ src/main/java/guru/sfg/brewery/eureka/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package guru.sfg.brewery.eureka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by fabreu on 05/12/2021
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+    
+}
